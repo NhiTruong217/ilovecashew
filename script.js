53dadeaed4c9f76ec8781e8150b6b66c1d307d35
Nhi Truong <luyen02081975@gmail.com>
2025-05-31 14:49:19 -0700

    basic javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+let name = "Nhi";
+let age = 14;
+let likesushi = false;
+
+function greeting(name) {
+    console.log("Hello", name);
+}
+
+greeting("Nhi");
+
+let newName = document.getElementById("greeter").value;
