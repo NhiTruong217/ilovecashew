0cfc726ad1909f02443295264c92096418bb732f
Nhi Truong <luyen02081975@gmail.com>
2025-06-21 11:50:20 -0700

    edited password button

--- script.js
+++ script.js
@@ -20,3 +20,21 @@ document.querySelectorAll('.accordion-header').forEach(function (button) {
         content.style.display = (content.style.display === 'block') ? 'none' : 'block'; 
     });
 });
+
+const PASSWORD = "*****";
+
+function validatePass() {
+    const login = document.getElementById('password');
+    const message = document.getElementById('message');
+    const hiddenEntries = document.querySelectorAll('.hidden');
+
+    if (login.value === PASSWORD) {
+        message.textContent = 'Password confirmed. See hidden entries below';
+        hiddenEntries.forEach(function(entry) {entry.style.display ='flex';});
+    } else {
+        message.textContent = 'Password denied. No free cashew';
+        hiddenEntries.forEach(function(entry) {entry.style.display ='none';}); 
+    }
+
+    login.value = '';
+}
